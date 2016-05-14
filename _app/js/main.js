ca5962bfbc175451d76ce16ffd3a93835276ff3e
budparr <budparr@sonnetmedia.net>
2016-05-14 10:15:27 -0400

    convert nav to vanilla
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,19 +1,4 @@
-// First we detect the click event
-document.getElementById('js-mobile-menu').addEventListener('click', function () {
-
-  var menu = document.getElementById('navigation-menu');
-  // Using an if statement to check the class
-  if (menu.classList.contains('show')) {
-    // The box that we clicked has a class of bad so let's remove it and add the good class
-      menu.classList.remove('show');
-      menu.classList.add('hide');
-  } else {
-    // The user obviously can't follow instructions so let's alert them of what is supposed to happen next
-    //alert("You can proceed!");
-  }
-});
-
-
+window.onload = function() {
 
 imagesLoaded( document.querySelector('#image-container'), function( instance ) {
   var elem = document.querySelector('.grid');
@@ -23,21 +8,15 @@ imagesLoaded( document.querySelector('#image-container'), function( instance ) {
     columnWidth: '.grid-sizer',
     percentPosition: true
   });
-//console.log('images have loaded')
 });
 
+var menuToggle = document.getElementById('js-mobile-menu');// may still need something for unbind
 
+    var menu = document.getElementById('navigation-menu');
 
-// $(document).ready(function() {
-//   var menuToggle = $("#js-mobile-menu").unbind();
-//   $("#js-navigation-menu").removeClass("show");
+    menuToggle.addEventListener('click', function () {
+        //e.preventDefault();
+        menu.classList.toggle('show');
+    });
 
-//   menuToggle.on("click", function(e) {
-//     e.preventDefault();
-//     $("#js-navigation-menu").slideToggle(function(){
-//       if($("#js-navigation-menu").is(":hidden")) {
-//         $("#js-navigation-menu").removeAttr("style");
-//       }
-//     });
-//   });
-// });
+}; // window.onload
