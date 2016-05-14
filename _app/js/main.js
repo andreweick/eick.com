9c170180ebd1df4442db3ef83d6ff9721d33fbc0
budparr <budparr@sonnetmedia.net>
2016-05-14 10:26:59 -0400

    clean up script
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,22 +1,19 @@
-window.onload = function() {
+window.onload = function () {
+    'use strict';
+    imagesLoaded(document.querySelector('#image-container'), function (instance) {
+        var elem = document.querySelector('.grid');
+        var msnry = new Masonry(elem, {
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
+    });
 
-imagesLoaded( document.querySelector('#image-container'), function( instance ) {
-  var elem = document.querySelector('.grid');
-  var msnry = new Masonry( elem, {
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
-});
-
-var menuToggle = document.getElementById('js-mobile-menu');// may still need something for unbind
+    var menuToggle = document.getElementById('js-mobile-menu');// may still need something for unbind
 
     var menu = document.getElementById('navigation-menu');
 
     menuToggle.addEventListener('click', function () {
-        //e.preventDefault();
         menu.classList.toggle('show');
     });
-
 }; // window.onload
