e447621dc17bd9a845b55c673d7e7d13ecdcd214
budparr <budparr@gmail.com>
2019-03-21 19:27:10 -0400

    [wip] Clean up scripts

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,10 @@
 require("typeface-play")
 import "./css/styles.css"
+
+import "./js/polyfills.js"; //MUST GO AT TOP
 import "./js/lazysizes.js"
+import "./js/quicklink.js"
 import "./js/nojs.js"
+
+
+console.log("NODE_ENV=", process.env.NODE_ENV);
