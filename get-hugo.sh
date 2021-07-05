f000024cf1ed8a27d6acc8082dd745d9bf52a9e5
M. Andrew Eick <maeick@missionfocus.com>
2021-07-05 19:01:25 +0000

    get-hugo now OS, Architecture and version aware

--- get-hugo.sh
+++ get-hugo.sh
@@ -13,6 +13,24 @@ fi
 
 FILE=hugo_${HUGO_VERSION}
 
+# Try to decode the architecture from the arch command.  I'm not sure what apple silicon returns
+
+arch=`arch`
+case $arch in
+    arm64 | aarch64) 
+        ARCH="ARM64"
+        ;;
+    x86_64 | i386)
+        ARCH="64bit"
+        ;;
+    arm32 | armhf)
+        ARCH="ARM"
+        ;;
+    *)
+        echo Your machine kernel architecture $arch is unkown and not supported by the script
+        exit 1
+esac
+
 set -e
 
 if [ -n $HUGO_VERSION ]; then
@@ -38,29 +56,47 @@ fi
 
 echo Make temp directory
 
-mkdir -p ${PWD}/temp ${PWD}/bin
+TEMPDIR=$(mktemp -d) || { echo "Failed to create temp directory"; exit 1; }
 
-echo Move to temp directory
+if [ -f "/etc/os-release" ]; then
+    # Linux of some flavor.  If I need to add specific detection, this is how I might do it
+    # Hugo downloads make a different download for BSD
+    OSVER=`grep '^ID=' /etc/os-release`
+    case $OSVER in
+        ID=debian)
+            OS=Linux
+            ;;
+        ID="centos")
+            OS=Linux
+            ;;
+    esac
+else
+    OS=`sw_vers -productName`       #macOS on my mac
+fi
 
-pushd ${PWD}/temp
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${EXTENDED}${HUGO_VERSION}_${OS}-${ARCH}.tar.gz --output ${TEMPDIR}/hugo_${EXTENDED}${HUGO_VERSION}_${OS}-${ARCH}.tar.gz
 
-echo Current Directory: ${PWD}
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_checksums.txt --output ${TEMPDIR}/checksums.txt
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${EXTENDED}${HUGO_VERSION}_macOS-64bit.tar.gz -o ${FILE}.tar.gz
+# Get the checksum and see if checksum is listed anywhere in the checksum file
+CHECKSUM=$(shasum --algorithm 256 ${TEMPDIR}/hugo_${EXTENDED}${HUGO_VERSION}_${OS}-${ARCH}.tar.gz | cut -f1 -d' ')
 
-TARBALL=${FILE}.tar.gz
+ISCHECKSUMFOUND=`grep ${CHECKSUM} ${TEMPDIR}/checksums.txt`
 
-echo Extracting $TARBALL
+if [ -z "$ISCHECKSUMFOUND" ]; then
+    echo "CHECKSUM not found ... exiting"
+    exit
+fi
 
-tar --extract --file $TARBALL
+
+tar --extract --file ${TEMPDIR}/*.tar.gz --directory ${TEMPDIR}
 
 # do we need sudo command for these?
-chmod +x hugo
+chmod +x ${TEMPDIR}/hugo
 
-mv hugo $DIR/bin/${FILE}
+mv ${TEMPDIR}/hugo $DIR/bin/${FILE}
 
-# Clean up temp directory (popd) ?
-rm -rf $DIR/temp
+# The OS will delete the temporary directory at some point
 
 location="$(which $DIR/bin/${FILE})"
 echo "Hugo binary location: $location"
