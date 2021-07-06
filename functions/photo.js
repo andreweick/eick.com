a7725b3a7b11c2429bea845beee7b6fa1ba27ba4
Regis Philibert <login@regisphilibert.com>
2021-07-06 09:34:59 -0400

    Update photo.js to use builder
    Use beautiful URL instead of query parameters
    
    Fixes #116

--- functions/photo.js
+++ functions/photo.js
@@ -1,5 +1,11 @@
 const { builder } = require("@netlify/functions")
 
+/**
+ * Extract data from the Netlify provided context 
+ * @param {*} context 
+ * @returns Map
+ *   String (.site_url)
+ */
 const extractNetlifySiteFromContext = function(context) {
   let site = {site_url: "http://localhost:8888"}
   if(typeof context.clientContext.custom !== "undefined") {
@@ -8,13 +14,26 @@ const extractNetlifySiteFromContext = function(context) {
   return site
 }
 
+/**
+ * Retrieves the last part of the URL after having removed any trailing slash
+ * @param {*} event 
+ * @returns String
+ */
+const getURLSlug = function(url) {
+  // Trim potential trailing slash and split
+  const URLSplits = url.replace(/\/$/, "").split('/');
+  const slug = URLSplits[URLSplits.length - 1]
+  return slug
+}
+
 const fetch = require("node-fetch");
 
 async function handler(event, context) {
   const {site_url} = extractNetlifySiteFromContext(context)
 
-  const name = event.queryStringParameters.name
+  const name = getURLSlug(event.path)
   
+  /** 1. First we grab the HTML from the Hugo generated placeholder */
   const placeholder = await fetch(`${site_url}/placeholder/index.html`).then(response => {
     if(response.ok) {
       return response.text()
@@ -22,15 +41,19 @@ async function handler(event, context) {
       throw new Error(`Nothing found at ${site_url}/placeholder/index.html`);
     }
   })
-  const photo_data = await fetch(`https://m2gfewcae1.execute-api.us-east-1.amazonaws.com/Prod/photoapi?name=${name}`).then(response => {
+  /** 2. Then we fetch the data from the API */
+  const photo_data = await fetch(`${process.env.S3API_URL}?name=${name}`).then(response => {
     if(response.ok) {
       return response.json()
     }
   })
-  let content = await placeholder.replace(/builder_eick_title/g, name)
-                              .replace(/builder_eick_img_src/g, name)
-                              .replace(/builder_eick_debug/g, event.queryStringParameters.name)
-                              .replace(/builder_eick_artist/g, photo_data.Artist)
+
+  let content = await placeholder
+    .replace(/builder_eick_title/g, name)
+    .replace(/builder_eick_img_src/g, name)
+    .replace(/builder_eick_debug/g, event.path)
+    .replace(/builder_eick_artist/g, photo_data.Artist)
+
   return {
     statusCode: 200,
     headers: {
@@ -44,7 +67,7 @@ async function handler(event, context) {
 // As builder is not supported locally, we need to comment/uncomment for now.
 
 //For deploy:
-//exports.handler = builder(handler)
+exports.handler = builder(handler)
 
 // For local
-exports.handler = handler
+//exports.handler = handler
