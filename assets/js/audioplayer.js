ad61a34488b8fae845ff253b4e89cfe9a6e4d2b2
Regis Philibert <login@regisphilibert.com>
2021-07-28 15:48:15 -0400

    Add multiple songs UX to audio shortcode
    Improve audioplayer to cate for "playlist", several tracks
    
    Fixes #123

--- assets/js/audioplayer.js
+++ assets/js/audioplayer.js
@@ -1,13 +1,12 @@
 var Amplitude = require("amplitudejs")
 
-const player = document.getElementById('audioplayer-0')
+const player = document.querySelector('[data-audioplayer]')
 //const player = document.querySelector('data-audioplayer')
 if (player) {
 
   const songs = player.getAttribute('data-songs')
   Amplitude.init({
     songs: JSON.parse(songs),
-    debug: true
   })
   
   var progress = document.getElementById("song-played-progress")
---
+++ content/multiple-songs.md
@@ -0,0 +1,14 @@
+---
+title: Multiple songs
+private: true
+---
+
+
+### Multiple songs
+
+{{< audio title="Libby Piano Run 1, Libby Piano Run 2, Libby Piano Drill" file="https://cdn.eick.com/audio/2021-07-10-run-1.mp3, https://cdn.eick.com/audio/2021-07-10-run-2.mp3, https://cdn.eick.com/audio/2021-07-10-drill-1.mp3" >}}
+
+
+
+
+
