3f1e2a6f0408d7ec94b278fbc2115652b7dd5b7a
Regis Philibert <login@regisphilibert.com>
2021-08-02 09:51:54 -0400

    Add mux shortcode and JS logic (#143)
    * Add mux shortcode and JS logic
    
    Fixes #125
    
    * Remove -e cached from deploy URL as well
    
    * tailwind: remove unused plugin
    
    * Fix md
    
    * Add title param to shortcode
    Add expire-data param to shortcode
    
    Clean up a bit
    
    * Add poster signed srcThumbnail to video html element

---
+++ assets/js/mux.js
@@ -0,0 +1,59 @@
+async function getMuxData(id, expire_time, private = true) {
+  if(private) {
+    const response = await fetch(`/.netlify/functions/sign-video-url?id=${id}&expire_time=${expire_time}`)
+    if(response.ok) {
+      return response.json()
+    } else {
+      throw new Error(`We could not retrieve data for Mux private video (${id})`);
+    }
+  } else {
+    return {
+      src: id,
+      srcThumbnail: "https://image.mux.com/" + id + "/thumbnail.png?time=16",
+    }
+  }
+
+}
+const errorClasses = ['border', 'border-red', 'bg-red-lightest', 'text-red', 'p-4']
+const players = document.querySelectorAll('[data-mux-player]')
+if(players) {
+  const muxPlayers = {}
+  players.forEach(player => {
+    const [playerID, playerMuxID, expireTime, video_title] = [
+      player.getAttribute('id'),
+      player.getAttribute('data-video-id'),
+      player.getAttribute('data-expire-time') || '2h',
+      player.getAttribute('data-title')
+    ]
+    if(playerMuxID){
+      getMuxData(playerMuxID, expireTime)
+      .then(data => {
+        muxPlayers[playerMuxID] = videojs(playerID, {
+          plugins: {
+            mux: {
+              debug: false,
+              data: {
+                video_title
+              }
+            }
+          }
+        })
+        muxPlayers[playerMuxID].src({
+          src: data.src,
+          type: "video/mux",
+        });
+        muxPlayers[playerMuxID].timelineHoverPreviews({
+          enabled: true,
+          src: data.srcThumbnail
+        });
+        player.setAttribute('poster', data.srcThumbnail)
+      })
+      .catch(error => {
+        let errorBox = document.createElement('div')
+        errorBox.classList.add(...errorClasses)
+        errorBox.innerHTML = error
+        player.parentNode.replaceChild(errorBox, player);
+      });
+    }
+  })
+}
--- functions/sign-video-url.js
+++ functions/sign-video-url.js
@@ -1,48 +1,53 @@
 const Mux = require('@mux/mux-node');
 
-const playbackId = 'elecMd56a702qV7pWUCPO2Pejf01RvuKKF9EBrts7jdrk'; // This should be passed in as playBackID
-
-// Public and private keys for MUX -- the private key is stored in environment variable
-const keyIdPublic = process.env.MUX_ENCRYPT_PUBLIC_KEY
-const keyEncryptSecretEnv = process.env.MUX_ENCRYPT_PRIVATE_KEY                    
-
-const tokenValidFor = "2h"
-
-let baseOptions = {
-  keyId: keyIdPublic,     // Enter your signing key id here
-  keySecret: keyEncryptSecretEnv, // Enter your base64 encoded private key here
-};
-
-const token = Mux.JWT.sign(playbackId, { 
-    ...baseOptions, 
-    type: 'video', // Valid types are video, thumbnail, gif, or storyboard
-    expiration: tokenValidFor, 
-});
-
-const tokenStoryboard = Mux.JWT.sign(playbackId, { 
-    ...baseOptions, 
-    type: 'storyboard', // Valid types are video, thumbnail, gif, or storyboard
-    expiration: tokenValidFor, 
-});
-
-const tokenThumbnail = Mux.JWT.sign(playbackId, { 
-    ...baseOptions, 
-    type: 'thumbnail', // Valid types are video, thumbnail, gif, or storyboard
-    expiration: tokenValidFor, 
-});
-
-
 exports.handler = async function(event, context) {
+
+    //const playbackId = 'elecMd56a702qV7pWUCPO2Pejf01RvuKKF9EBrts7jdrk'; // This should be passed in as playBackID
+    const playbackID = event.queryStringParameters.id
+    // Public and private keys for MUX -- the private key is stored in environment variable
+    const keyIdPublic = process.env.MUX_ENCRYPT_PUBLIC_KEY
+    const keyEncryptSecretEnv = process.env.MUX_ENCRYPT_PRIVATE_KEY                    
+
+    let tokenValidFor = "2h"
+    if(event.queryStringParameters.expire_time){
+        tokenValidFor = event.queryStringParameters.expire_time
+    }
+    let baseOptions = {
+        keyId: keyIdPublic,     // Enter your signing key id here
+        keySecret: keyEncryptSecretEnv, // Enter your base64 encoded private key here
+    };
+
+    const token = Mux.JWT.sign(playbackID, { 
+        ...baseOptions, 
+        type: 'video', // Valid types are video, thumbnail, gif, or storyboard
+        expiration: tokenValidFor, 
+    });
+
+    const tokenStoryboard = Mux.JWT.sign(playbackID, { 
+        ...baseOptions, 
+        type: 'storyboard', // Valid types are video, thumbnail, gif, or storyboard
+        expiration: tokenValidFor, 
+    });
+
+    const tokenThumbnail = Mux.JWT.sign(playbackID, { 
+        ...baseOptions, 
+        type: 'thumbnail', // Valid types are video, thumbnail, gif, or storyboard
+        expiration: tokenValidFor, 
+    });
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-            src: playbackId + "?token=" + token, 
-            srcHls: "https://stream.mux.com/" + playbackId + ".m3u8?token=" + token,
-            srcTimelineHover: "https://image.mux.com/" + playbackId + "/storyboard.vtt?token=" + tokenStoryboard,  
-            srcStoryboard: "https://image.mux.com/" + playbackId + "/storyboard.jpg?token=" + tokenStoryboard,
-            token: token, 
+            src: playbackID + "?token=" + token, 
+            srcThumbnail: "https://image.mux.com/" + playbackID + "/thumbnail.jpg?token=" + tokenThumbnail,
+            // For debugging purposes
+            tokenValidFor,
+            // More data
+            srcHls: "https://stream.mux.com/" + playbackID + ".m3u8?token=" + token,
+            srcTimelineHover: "https://image.mux.com/" + playbackID + "/storyboard.vtt?token=" + tokenStoryboard,  
+            srcStoryboard: "https://image.mux.com/" + playbackID + "/storyboard.jpg?token=" + tokenStoryboard,
+            token: token,
             tokenStoryboard: tokenStoryboard, 
-            srcThumbnail: "https://image.mux.com/" + playbackId + "/thumbnail.jpg?token=" + tokenThumbnail,
             tokenTumbnail: tokenThumbnail
         })
     };
